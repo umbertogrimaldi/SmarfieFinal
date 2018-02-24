c69dae0766159c7227a5f4b49ec846448b41924e
Antonio Cerqua <ntncrq7@gmail.com>
2018-02-24 14:45:01 +0100

    add corredata model

--- SmarfieFinal/AppDelegate.swift
+++ SmarfieFinal/AppDelegate.swift
@@ -41,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        
+        PersistenceService.saveContext()
     }
 
 }
