6aa12f89d0b2443e73b559aad2004511427068b0
MicheleDeSena <micheledes1296@gmail.com>
2018-02-25 14:33:36 +0100

    final changes

--- SmarfieFinal/PreviewController/PhotoClassifier.swift
+++ SmarfieFinal/PreviewController/PhotoClassifier.swift
@@ -34,7 +34,7 @@ class PhotoClassifier {
     var score:Double = 0.0
    
     var goodPhotoInfo = "Wow! "
-    var perfect:Bool = true
+    
     
     
     func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage {
@@ -56,7 +56,7 @@ class PhotoClassifier {
         let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
         let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
         let options = [CIDetectorSmile:true,CIDetectorEyeBlink:true,CIDetectorFocalLength:true]
-let faces = faceDetector?.features(in:newImage, options: options)
+        let faces = faceDetector?.features(in:newImage, options: options)
         return faces as! [CIFaceFeature]
     }
     
@@ -71,6 +71,7 @@ let faces = faceDetector?.features(in:newImage, options: options)
         score = 0.0
         var photoInfo = "Hint: "
         goodPhotoInfo = "Wow! "
+        var perfect:Bool = true
         
         let brightness = image.image.brightness
         
@@ -85,13 +86,13 @@ let faces = faceDetector?.features(in:newImage, options: options)
                 self.smile = face.hasSmile
                 self.shutEyes = !face.leftEyeClosed && !face.rightEyeClosed
                 
-                if face.faceAngle > -15 || face.faceAngle < 15 {
+                if face.faceAngle > -15 && face.faceAngle < 15 {
                     self.faceAngleScore = abs(Double(face.faceAngle) / 15)
-                    self.faceAngleScore = 1 - self.faceAngleScore
+//                    self.faceAngleScore = 1 - self.faceAngleScore
                     faceScore += self.faceAngleScore
                     
                 }else{
-                    photoInfo += "straighten up your head!"
+                    photoInfo += "straighten up your head! \n"
                     perfect = false
                 }
                 
@@ -99,7 +100,7 @@ let faces = faceDetector?.features(in:newImage, options: options)
                     faceScore += 1.0
                     goodPhotoInfo += "What a great smile!"
                 }else {
-                    photoInfo += "Come on dude, smile!\n"
+                    photoInfo += "Smile more!\n"
                     perfect = false
                 }
                 
@@ -107,7 +108,7 @@ let faces = faceDetector?.features(in:newImage, options: options)
                     faceScore += 1.0
                     goodPhotoInfo += "Your eyes are beautiful\n"
                 }else {
-                     photoInfo += "Open your eyes, they're so shiny!\n"
+                     photoInfo += "Open your eyes! \n"
                      perfect = false
                 }
             }
@@ -131,7 +132,7 @@ let faces = faceDetector?.features(in:newImage, options: options)
                     faceScore += 1.0
                     goodPhotoInfo += "you're on your best side\n"
                 }else {
-                    photoInfo += "try to take a selfie of your best side \n"
+                    photoInfo += "try to turn on your best side \n"
                     perfect = false
                 }
             }
@@ -140,12 +141,12 @@ let faces = faceDetector?.features(in:newImage, options: options)
             
             if brightness > 34 && brightness < 227 {
                 bscore = (brightness - 131) / 96
-                bscore = 1 - bscore
-            }else if brightness < 34{
-                photoInfo += "you're in a dark place, try to increase brightness\n"
+//                bscore = 1 - bscore
+            }else if brightness < 30{
+                photoInfo += "Increase brightness\n"
                 perfect = false
-            }else if brightness > 227{
-                photoInfo += "you're too bright man, stop shining or get in a darker place \n "
+            }else if brightness > 230{
+                photoInfo += " Decrease brightness \n "
                 perfect = false
             }
             
@@ -178,7 +179,7 @@ let faces = faceDetector?.features(in:newImage, options: options)
             photoInfo = "You're too much guys, I can't give you any hint but i'm pretty shure you're really really beautiful"
         }
         
-        
+        print (photoInfo)
         return (totalScore,photoInfo)
     }
     
